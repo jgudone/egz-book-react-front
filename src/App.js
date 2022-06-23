116e116bad8f799f72ae5e416aeabb381f298be6
jgudone <95044532+jgudone@users.noreply.github.com>
2022-06-23 14:02:48 +0300

    initial

--- src/App.js
+++ src/App.js
@@ -1,25 +1,40 @@
-import logo from './logo.svg';
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.css';
+import * as React from "react";
+import Navbar from "react-bootstrap/Navbar";
+import Container from "react-bootstrap/Container";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
+
+import { BrowserRouter as Router , Routes, Route, Link } from "react-router-dom";
+
+import CreateBook from "./components/CreateBook";
+import UpdateBook from "./components/UpdateBook";
+import BookList from "./components/BookList";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  return (<Router>
+    <Navbar bg="primary">
+      <Container>
+        <Link to={"/"} className="navbar-brand text-white">
+          Book
+        </Link>
+      </Container>
+    </Navbar>
+
+    <Container className="mt-5">
+      <Row>
+        <Col md={12}>
+          <Routes>
+            <Route path="/book/create" element={<CreateBook />} />
+            <Route path="/book/update/:id" element={<UpdateBook />} />
+            {/* <Route exact path='/' element={<BookList />} /> */}
+            <Route path="/" element={<BookList />} />
+          </Routes>
+        </Col>
+      </Row>
+    </Container>
+  </Router>);
 }
 
 export default App;
